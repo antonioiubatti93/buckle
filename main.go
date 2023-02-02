02ff6091ccccaeece725d319081eaf3d0085e617
AntonioIubatti <aiubatti@edgelab.ch>
2023-02-02 16:59:04 +0100

    add example with marketdata curve

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/antonioiubatti93/buckle/interestrate"
+	"github.com/antonioiubatti93/buckle/marketdata"
 	"github.com/antonioiubatti93/buckle/rate"
 	"github.com/antonioiubatti93/buckle/termstructure"
 )
@@ -27,7 +29,16 @@ func main() {
 
 	fmt.Println("forward rate at 1y:", rate.Compute(1.0))
 
-	ir := interestrate.New(termstructure.NewConstant(0.01))
+	var curve marketdata.InterestRateCurve
+	if err := json.Unmarshal([]byte(`{
+		"M1": 0.01,
+		"M6": 0.02,
+		"Y1": 0.03
+	}`), &curve); err != nil {
+		log.Fatal(err)
+	}
+
+	ir := interestrate.New(curve.TermStructure())
 
 	fmt.Println("discount at 1y:", ir.Discount(1.0))
 }
